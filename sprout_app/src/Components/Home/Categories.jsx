65788c0d7a68db557d893533a76566d0c6e97217
do-ko <doko@onet.eu>
2024-02-12 21:58:50 +0100

    categories responsibility added

--- sprout_app/src/Components/Home/Categories.jsx
+++ sprout_app/src/Components/Home/Categories.jsx
@@ -4,22 +4,22 @@ import './Categories.css';
 
 export const Categories = () => {
     return (
-        <Container className={"sectionContainer"} style={{backgroundColor: "red"}}>
+        <Container className={"sectionContainer"} style={{height: "100vh"}}>
             <Row className={"row-cols-1 row-cols-lg-2"} style={{width: "80%", height: "70%"}}>
                 <Col lg={8}>
                     <Stack gap={3} style={{height: "100%"}}>
-                        <div style={{height: "70%"}}
-                             className={"imageCategory imagePlants textCategory"}>
+                        <div className={"imageCategory imagePlants textCategory"}>
                             Plants
                         </div>
-                        <div style={{height: "30%"}}
-                             className={"imageCategory imageGround textCategory"}>
+                        <div className={"imageCategory imageGround textCategory"}>
                             Ground
                         </div>
                     </Stack>
                 </Col>
-                <Col lg={4} className={"imageCategory imagePots textCategory"}>
-                    Pots
+                <Col lg={4}>
+                    <div className={"imageCategory imagePots textCategory"}>
+                        Pots
+                    </div>
                 </Col>
             </Row>
         </Container>
