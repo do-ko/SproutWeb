b59b2f007cf06e55daf8ced70cfb975a238b1b13
do-ko <doko@onet.eu>
2024-02-13 20:26:33 +0100

    carousel functionality and responsibility added

--- sprout_app/src/Components/Home/TopPlants.jsx
+++ sprout_app/src/Components/Home/TopPlants.jsx
@@ -1,10 +1,11 @@
-import {Button, Card, Carousel, Col, Container, Row} from "react-bootstrap";
+import {Card, Carousel, Container} from "react-bootstrap";
 import './TopPlants.css';
 import plant1 from '../../assets/topPlants/plant1.jpg';
 import plant2 from '../../assets/topPlants/plant2.jpg';
 import plant3 from '../../assets/topPlants/plant3.jpg';
-import {createRef, useState} from "react";
-import { useRef } from 'react';
+import plant4 from '../../assets/topPlants/plant4.jpg';
+import plant5 from '../../assets/topPlants/plant5.jpg';
+import {useRef, useState} from "react";
 
 
 export const TopPlants = () => {
@@ -18,11 +19,11 @@ export const TopPlants = () => {
     // const testRef = createRef();
 
     const slideFunction = (eventNumber, direction) => {
-        if (direction==="start"){
-            if (scrollPosition < 4-3){
-                setScrollPosition((scrollPosition)=>scrollPosition+1)
+        if (direction === "start") {
+            if (scrollPosition < 4 - 3) {
+                setScrollPosition((scrollPosition) => scrollPosition + 1)
             }
-            switch (scrollPosition){
+            switch (scrollPosition) {
                 case 0:
                     myRef3.current.scrollIntoView({
                         behavior: 'smooth',
@@ -38,11 +39,11 @@ export const TopPlants = () => {
                     })
                     return;
             }
-        } else if (direction==="end"){
-            if (scrollPosition > 0){
-                setScrollPosition((scrollPosition)=>scrollPosition-1)
+        } else if (direction === "end") {
+            if (scrollPosition > 0) {
+                setScrollPosition((scrollPosition) => scrollPosition - 1)
             }
-            switch (scrollPosition){
+            switch (scrollPosition) {
                 case 0:
                     myRef1.current.scrollIntoView({
                         behavior: 'smooth',
@@ -65,50 +66,77 @@ export const TopPlants = () => {
         <div className={"top-plants-background"}>
             <Container>
                 <h3 className={"primary-title top-plants-title"}>Top plants</h3>
-                <Carousel slide={false} interval={null} pause={"hover"} onSlide={(eventKey, direction) => slideFunction(eventKey, direction)}>
+                <Carousel slide={false} interval={null} pause={"hover"}
+                          onSlide={(eventKey, direction) => slideFunction(eventKey, direction)}>
                     <Carousel.Item>
                         <Card ref={myRef1}>
-                            {/*<div className={"imageWrapper"}>*/}
-                            <Card.Img variant={"top"} src={plant1}/>
-                            {/*</div>*/}
+                            <div className={"imageWrapper plant1"}>
+                                <Card.Img variant={"top"} src={plant1}/>
+                            </div>
                             <Card.Body>
-                                <Card.Title>Card Title1</Card.Title>
+                                <Card.Title>Philodendron</Card.Title>
+                                <div className={"plantTagContainer"}>
+                                    <div className={"plantTag toxicTag"}>TOXIC</div>
+                                    <div className={"plantTag sunTag"}>SUN</div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Carousel.Item>
                     <Carousel.Item>
                         <Card ref={myRef2}>
-                            <Card.Img variant={"top"} src={plant2}/>
+                            <div className={"imageWrapper plant2"}>
+                                <Card.Img variant={"top"} src={plant2}/>
+                            </div>
                             <Card.Body>
-                                <Card.Title>Card Title2</Card.Title>
+                                <Card.Title>Monstera</Card.Title>
+                                <div className={"plantTagContainer"}>
+                                    <div className={"plantTag wetTag"}>WET</div>
+                                    <div className={"plantTag sunTag"}>SUN</div>
+                                    <div className={"plantTag petTag"}>PET</div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Carousel.Item>
                     <Carousel.Item>
                         <Card ref={myRef3}>
-                            <Card.Img variant={"top"} src={plant3}/>
+                            <div className={"imageWrapper plant3"}>
+                                <Card.Img variant={"top"} src={plant3}/>
+                            </div>
                             <Card.Body>
-                                <Card.Title>Card Title3</Card.Title>
+                                <Card.Title>Aloe Vera</Card.Title>
+                                <div className={"plantTagContainer"}>
+                                    <div className={"plantTag dryTag"}>DRY</div>
+                                    <div className={"plantTag sunTag"}>SUN</div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Carousel.Item>
                     <Carousel.Item>
                         <Card ref={myRef4}>
-                            {/*<div className={"imageWrapper"}>*/}
-                            <Card.Img variant={"top"} src={plant1}/>
-                            {/*</div>*/}
+                            <div className={"imageWrapper plant4"}>
+                                <Card.Img variant={"top"} src={plant4}/>
+                            </div>
                             <Card.Body>
-                                <Card.Title>Card Title4</Card.Title>
+                                <Card.Title>Musa</Card.Title>
+                                <div className={"plantTagContainer"}>
+                                    <div className={"plantTag sunTag"}>SUN</div>
+                                    <div className={"plantTag dryTag"}>DRY</div>
+                                    <div className={"plantTag petTag"}>PET</div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Carousel.Item>
                     <Carousel.Item>
                         <Card ref={myRef0}>
-                            {/*<div className={"imageWrapper"}>*/}
-                            <Card.Img variant={"top"} src={plant1}/>
-                            {/*</div>*/}
+                            <div className={"imageWrapper plant5"}>
+                                <Card.Img variant={"top"} src={plant5}/>
+                            </div>
                             <Card.Body>
-                                <Card.Title>Card Title5</Card.Title>
+                                <Card.Title>Bird's Nest Fern</Card.Title>
+                                <div className={"plantTagContainer"}>
+                                    <div className={"plantTag petTag"}>PET</div>
+                                    <div className={"plantTag wetTag"}>WET</div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Carousel.Item>
