155d9783d5ab25115005d48e26add57815fd9b2d
do-ko <doko@onet.eu>
2024-02-17 17:07:23 +0100

    navigation links update

--- sprout_app/src/Components/Home/Navigation.jsx
+++ sprout_app/src/Components/Home/Navigation.jsx
@@ -1,26 +1,40 @@
 import Navbar from 'react-bootstrap/Navbar';
-import {Col, Container, Nav, Row} from "react-bootstrap";
+import {Container, Nav} from "react-bootstrap";
 import "./Navigation.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleUser} from "@fortawesome/free-regular-svg-icons";
+import {faBars, faCartShopping} from "@fortawesome/free-solid-svg-icons";
+
 export const Navigation = () => {
-    return(
+    return (
         <Navbar id={"navigation-custom"} sticky={"top"} expand={"lg"}>
             <Container fluid={"md"}>
-                <Navbar.Brand>Sprout</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Brand className={"brand-name-large"}>Sprout</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <FontAwesomeIcon icon={faBars} />
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav" className={"collapsable-menu"}>
                     <Nav>
-                        <Nav.Link>plants</Nav.Link>
-                        <Nav.Link>ground</Nav.Link>
-                        <Nav.Link>pots</Nav.Link>
+                        <Container className={"store-links-container"}>
+                            <Nav.Link>plants</Nav.Link>
+                            <Nav.Link>ground</Nav.Link>
+                            <Nav.Link>pots</Nav.Link>
+                        </Container>
+
                     </Nav>
                 </Navbar.Collapse>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <Navbar.Text>
-                            Signed in as: Mark Otto
-                        </Navbar.Text>
-                    </Nav>
-                </Navbar.Collapse>
+                <Navbar.Brand className={"brand-name-small"}>Sprout</Navbar.Brand>
+                <Nav className={"account-links-container"}>
+                    <Nav.Link className={"account-link"}>
+                        <FontAwesomeIcon icon={faCartShopping} />
+                        <p className={"link-text"}>Cart</p>
+                    </Nav.Link>
+                    <Nav.Link className={"account-link"}>
+                        <FontAwesomeIcon icon={faCircleUser}/>
+                        <p className={"link-text"}>Sign In</p>
+                    </Nav.Link>
+                </Nav>
+
             </Container>
         </Navbar>
     )
