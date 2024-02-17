2228ae3882a4403c555e93fef06201a926dea1c2
do-ko <doko@onet.eu>
2024-02-18 00:02:28 +0100

    logout function added

--- sprout_app/src/Components/Home/Navigation.jsx
+++ sprout_app/src/Components/Home/Navigation.jsx
@@ -4,11 +4,21 @@ import "./Navigation.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleUser} from "@fortawesome/free-regular-svg-icons";
 import {faArrowRightFromBracket, faBars, faCartShopping} from "@fortawesome/free-solid-svg-icons";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import AuthContext from "../../context/AuthProvider";
 
 export const Navigation = ({sticky, fixed}) => {
-    // const {auth, logout} = useContext(AuthContext);
+    const [isAuthorized, setIsAuthorized] = useState(false);
+    const {auth, logout} = useContext(AuthContext);
+
+    useEffect(() => {
+        if (Object.keys(auth).length === 0){
+            setIsAuthorized(false)
+        } else setIsAuthorized(true)
+        console.log("auth")
+        console.log(auth)
+    }, [auth])
+
     return (
         <Navbar id={"navigation-custom"} sticky={sticky} fixed={fixed} expand={"lg"}>
             <Container fluid={"md"}>
@@ -32,17 +42,18 @@ export const Navigation = ({sticky, fixed}) => {
                         <FontAwesomeIcon icon={faCartShopping}/>
                         <p className={"link-text"}>Cart</p>
                     </Nav.Link>
-                    <Nav.Link href={"/login"} className={"account-link"}>
-                        <FontAwesomeIcon icon={faCircleUser}/>
-                        <p className={"link-text"}>Sign In</p>
-                    </Nav.Link>
-                    {/*{auth === {} ? <Nav.Link href={"/login"} className={"account-link"}>*/}
+                    {/*<Nav.Link href={"/login"} className={"account-link"}>*/}
                     {/*    <FontAwesomeIcon icon={faCircleUser}/>*/}
                     {/*    <p className={"link-text"}>Sign In</p>*/}
-                    {/*</Nav.Link> : <Nav.Link href={"/"} className={"account-link"} onClick={logout}>*/}
-                    {/*    <FontAwesomeIcon icon={faArrowRightFromBracket} />*/}
-                    {/*    <p className={"link-text"}>Logout</p>*/}
-                    {/*</Nav.Link>}*/}
+                    {/*</Nav.Link>*/}
+                    {/*<Nav.Item>{isAuthorized ? "auth" : "not auth"}</Nav.Item>*/}
+                    {!isAuthorized ? <Nav.Link href={"/login"} className={"account-link"}>
+                        <FontAwesomeIcon icon={faCircleUser}/>
+                        <p className={"link-text"}>Sign In</p>
+                    </Nav.Link> : <Nav.Link href={"/"} className={"account-link"} onClick={logout}>
+                        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                        <p className={"link-text"}>Logout</p>
+                    </Nav.Link>}
 
                 </Nav>
 
