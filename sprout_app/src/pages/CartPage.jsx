e37a5f91843d3e7f4d4f3ff3431e86eeb307a16e
do-ko <doko@onet.eu>
2024-03-06 21:48:57 +0100

    distinguished carts on cartPage

--- sprout_app/src/pages/CartPage.jsx
+++ sprout_app/src/pages/CartPage.jsx
@@ -3,39 +3,67 @@ import {Navigation} from "../Components/Home/Navigation";
 import {useContext, useEffect, useState} from "react";
 import AuthContext from "../context/AuthProvider";
 import {useCookies} from "react-cookie";
-import {forEach} from "react-bootstrap/ElementChildren";
+import axios from "../api/axios";
 
 export const CartPage = () => {
     const {cart} = useContext(AuthContext);
+    // const {logout, getCart} = useContext(AuthContext);
     const [cookies, setCookie] = useCookies(['token', 'cart']);
     const [totalPrice, setTotalPrice] = useState(0);
-    useEffect(()=> {
-        let temp = 0;
-        if (cookies.token){
-            console.log(cart.plants);
-            // cart.plants.forEach((plantRecord) => {
-            //     temp += plantRecord.plant.price * plantRecord.count
-            // });
+    const [currentCart, setCurrentCart] = useState({})
+
+    const getCart = async () => {
+        return await axios.get("/api/cart",
+            {
+                headers: {
+                    "Authorization": ("Bearer " + cookies.token)
+                },
+            })
+    }
+
+    useEffect(() => {
+        //     switch cart when user logs in or logs out
+        if (cookies.token) {
+            console.log("Use logged in cart")
+            getCart().then(r => setCurrentCart(r.data))
         } else {
-            cookies.cart.forEach((item) => {
-                // console.log(item);
-                temp += item.price;
-            });
+            console.log("Use local cart")
+            setCurrentCart(JSON.parse(localStorage.getItem('cart')))
         }
-        // console.log(temp);
-        setTotalPrice(temp);
+    }, [cookies.token])
 
-    }, [cart, cookies.cart])
+    // useEffect(()=> {
+    //     // let temp = 0;
+    //     // if (cookies.token){
+    //     //     console.log(cart.plants);
+    //     //     // cart.plants.forEach((plantRecord) => {
+    //     //     //     temp += plantRecord.plant.price * plantRecord.count
+    //     //     // });
+    //     // } else {
+    //     //     cookies.cart.forEach((item) => {
+    //     //         // console.log(item);
+    //     //         temp += item.price;
+    //     //     });
+    //     // }
+    //     // console.log(temp);
+    //     // setTotalPrice(temp);
+    //
+    // }, [cart, cookies.cart])
 
-    return(
+    return (
         <>
             <Navigation/>
             <Container style={{backgroundColor: "red"}}>
                 <div>
                     <h3>Hi! This is your shopping cart!</h3>
-                    {totalPrice > 99 ? <p>Your order ships free!</p> : <p>Add {99-totalPrice}zł to get free shipping!</p>}
+                    {totalPrice > 99 ? <p>Your order ships free!</p> :
+                        <p>Add {99 - totalPrice}zł to get free shipping!</p>}
 
                 </div>
+                {console.log(currentCart)}
+                {currentCart.plants?.map(item => {
+                    return <p>{item.plant.name}</p>
+                })}
             </Container>
         </>
 
